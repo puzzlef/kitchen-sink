a99a03cd973d463cdf8f055f3cff1dcb77e3f548
Subhajit Sahu <wolfram77@gmail.com>
2022-01-12 17:39:26 +0530

    :turtle: w/ manual deduplication DiGraph

--- src/DiGraphCopy.hxx
+++ src/DiGraphCopy.hxx
@@ -12,8 +12,126 @@ using std::cout;
 
 
 
-// DI-GRAPH
-// --------
+// GRAPH (BASE)
+// ------------
+
+
+  // void correct() {
+  //   M = 0;
+  //   for (int u : vertices()) {
+  //     edata[u].correct();
+  //     M += edata[u].size();
+  //   }
+  // }
+
+
+template <class VD, class ED>
+class GraphBase {
+  // Data.
+  private:
+  ED none;
+  vector<bool> vexists;
+  vector<VD> vdata;
+  vector<ED> edata;
+  size_t N = 0;
+  size_t M = 0;
+
+
+  // Types.
+  private:
+  using K = typename ED::key_type;
+  using E = typename ED::value_type;
+  using V = VD;
+  public:
+  using key_type    = K;
+  using vector_type = V;
+  using edge_type   = E;
+
+
+  // Sizes.
+  public:
+  size_t span()  const { return vexists.size(); }
+  size_t order() const { return N; }
+  size_t size()  const { return M; }
+
+
+  // Read operations.
+  bool hasVertex(K u)    const { return u < span() && vexists[u]; }
+  bool hasEdge(K u, K v) const { return u < span() && edata[u].has(v); }
+  auto edges(K u)        const { return u < span()? edata[u].keys() : none.keys(); }
+  size_t degree(K u)     const { return u < span()? edata[u].size() : 0; }
+  auto vertices()      const { return filterIter(rangeIter(span()), [&](K u) { return  vexists[u]; }); }
+  auto nonVertices()   const { return filterIter(rangeIter(span()), [&](K u) { return !vexists[u]; }); }
+  auto inEdges(K v)    const { return filterIter(rangeIter(span()), [&](K u)   { return edata[u].has(v); }); }
+  size_t inDegree(K v) const { return    countIf(rangeIter(span()), [&](int u) { return edata[u].has(v); }); }
+
+  V       vertexData(int u)   const { return hasVertex(u)? vdata[u] : V(); }
+  void setVertexData(int u, V d)       { if (hasVertex(u)) vdata[u] = d; }
+  E       edgeData(int u, int v)   const { return hasEdge(u, v)? edata[u].get(v) : E(); }
+  void setEdgeData(int u, int v, E d)       { if (hasEdge(u, v)) edata[u].set(v, d); }
+
+  // Write operations
+  public:
+  void clear() {
+    vex.clear();
+    vdata.clear();
+    edata.clear();
+  }
+
+  void addVertex(int u, V d=V()) {
+    if (hasVertex(u)) return;
+    if (u >= span()) {
+      vex.resize(u+1);
+      vdata.resize(u+1);
+      edata.resize(u+1);
+    }
+    vex[u] = true;
+    vdata[u] = d;
+    N++;
+  }
+
+  void addEdgeUnchecked(int u, int v, E d=E()) {
+    addVertex(u);
+    addVertex(v);
+    edata[u].addUnchecked(v, d);
+    M++;
+  }
+
+  void addEdge(int u, int v, E d=E()) {
+    if (hasEdge(u, v)) return;
+    addVertex(u);
+    addVertex(v);
+    edata[u].add(v, d);
+    M++;
+  }
+
+  void removeEdge(int u, int v) {
+    if (!hasEdge(u, v)) return;
+    edata[u].remove(v);
+    M--;
+  }
+
+  void removeEdges(int u) {
+    if (!hasVertex(u)) return;
+    M -= degree(u);
+    edata[u].clear();
+  }
+
+  void removeInEdges(int v) {
+    if (!hasVertex(v)) return;
+    for (int u : inEdges(v))
+      removeEdge(u, v);
+  }
+
+  void removeVertex(int u) {
+    if (!hasVertex(u)) return;
+    removeEdges(u);
+    removeInEdges(u);
+    vex[u] = false;
+    N--;
+  }
+};
+
 
 template <class V=NONE, class E=NONE>
 class DiGraph {
@@ -79,8 +197,15 @@ class DiGraph {
     N++;
   }
 
+  void addEdgeUnchecked(int u, int v, E d=E()) {
+    addVertex(u);
+    addVertex(v);
+    edata[u].addUnchecked(v, d);
+    M++;
+  }
+
   void addEdge(int u, int v, E d=E()) {
-    // if (hasEdge(u, v)) return;
+    if (hasEdge(u, v)) return;
     addVertex(u);
     addVertex(v);
     edata[u].add(v, d);
